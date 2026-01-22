b11f543a9234dcf024c1963bef467a37ce0851c5
PegasusGirl <120068247+PegasusGirl@users.noreply.github.com>
2026-01-21 22:49:25 -0800

    Activate Python virtual environment in run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,5 @@
 echo on
+powershell .\venv_311\Scripts\Activate.ps1
 start /b venv_311\Scripts\python -m streamlit run page.py --theme.base="light" --server.port 8502
 timeout /t 4
 cloudflared tunnel --url http://localhost:8502
